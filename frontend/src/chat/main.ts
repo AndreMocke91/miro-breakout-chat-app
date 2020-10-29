00a52cd63037d7eee76f7dadfc6d034b7a78e1ed
AndreMocke91 <andremocke91@gmail.com>
2020-10-29 21:29:08 +0200

    Fixed indentation

--- frontend/src/chat/main.ts
+++ frontend/src/chat/main.ts
@@ -10,9 +10,9 @@ const initApp = (roomId: string, name: string, token: string, boardId: string) =
 		props: {
 			roomId,
 			name,
-      chatFactory: socketioControllerFactory,
-      token,
-      boardId
+			chatFactory: socketioControllerFactory,
+			token,
+			boardId
 		},
 	})
 }
@@ -27,14 +27,14 @@ const getCurrentUserName = async () => {
 
 const getBreakoutChatRoomId = async (): Promise<string> => {
 	const savedState = await miro.__getRuntimeState()
-  return savedState[CLIENT_ID] ? savedState[CLIENT_ID].breakoutChatRoomId : ''
+	return savedState[CLIENT_ID] ? savedState[CLIENT_ID].breakoutChatRoomId : ''
 }
 
 miro.onReady(async () => {
-  const breakoutChatRoomId = await getBreakoutChatRoomId()
-  const name = await getCurrentUserName()
-  const token = await miro.getToken()
-  const board = await miro.board.info.get()
+	const breakoutChatRoomId = await getBreakoutChatRoomId()
+	const name = await getCurrentUserName()
+	const token = await miro.getToken()
+	const board = await miro.board.info.get()
 
 	if (breakoutChatRoomId && name) {
 		initApp(breakoutChatRoomId, name, token, board.id)
