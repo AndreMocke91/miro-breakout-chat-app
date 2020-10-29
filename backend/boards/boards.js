cf7723a3f919549ee0a63a90e4b4de0339cd3d8f
AndreMocke91 <andremocke91@gmail.com>
2020-10-29 21:24:24 +0200

    Fixed formatting

--- backend/boards/boards.js
+++ backend/boards/boards.js
@@ -5,15 +5,14 @@ module.exports.boards = boards
 // this function should be deprecated or secured only for admin usages
 module.exports.getAllRooms = () => {
 	const allRooms = Object.entries(boards).reduce((prev, [boardId, rooms]) => {
-
-    Object.entries(rooms).forEach(([roomId, room]) => {
-      prev.push({
-        boardId,
-        roomId,
-        createdAt: room.createdAt,
-        users: Object.values(room.sockets).map(socket => socket.name)
-      })
-    })
+		Object.entries(rooms).forEach(([roomId, room]) => {
+			prev.push({
+				boardId,
+				roomId,
+				createdAt: room.createdAt,
+				users: Object.values(room.sockets).map((socket) => socket.name),
+			})
+		})
 
 		return prev
 	}, [])
